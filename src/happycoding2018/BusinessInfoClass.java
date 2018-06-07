57eaa9b653fdf3026fe1da37113403a20ed1a1fc
Jilin Liu <jilin@oomba.com>
2018-06-06 23:48:57 -0700

    yelp OA business info, sort by rating

---
+++ src/happycoding2018/BusinessInfoClass.java
@@ -0,0 +1,83 @@
+package happycoding2018;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+
+
+public class BusinessInfoClass {
+	
+	public static void main(String[] args) {
+		BusinessInfoClass bClass = new BusinessInfoClass();
+		BusinessInfo bi1 = bClass.new BusinessInfo(101, 5.2);
+		BusinessInfo bi2 = bClass.new BusinessInfo(102, 2);
+		BusinessInfo bi3 = bClass.new BusinessInfo(103, 3);
+		BusinessInfo bi4 = bClass.new BusinessInfo(104, 5.45);
+		BusinessInfo bi5 = bClass.new BusinessInfo(106, 5.32);
+		// Create list from array
+		
+		List<BusinessInfo> businesses = Arrays.asList(bi1, bi2, bi3, bi4, bi5);
+		List<BusinessInfo> sortedlist = bClass.sortBusinessesByRating(businesses);
+		for (BusinessInfo bi: sortedlist) {
+			System.err.println(bi.id + " " + bi.rating);
+		}
+	}
+	
+	
+	public class BusinessInfo {
+		int id;
+		double rating;
+		
+		public BusinessInfo(int id, double rating) {
+			this.id = id;
+			this.rating = rating;
+		}
+	}
+
+	public List<BusinessInfo> sortBusinessesByRating(List<BusinessInfo> businesses) {
+			PriorityQueue<BusinessInfo> queue = new PriorityQueue<>(new Comparator<BusinessInfo>() {
+
+				@Override
+				public int compare(BusinessInfo o1, BusinessInfo o2) {
+					if (o1.rating < o2.rating) {
+						return 1;
+					} else if (o1.rating > o2.rating) {
+						return -1;
+					}
+					return 0;
+				}
+			});
+			for (int i = businesses.size() - 1; i >= 0; i--) {
+				queue.offer(businesses.get(i));
+			}
+//			for (int i = 0; i < businesses.size(); i++) {
+//				queue.offer(businesses.get(i));
+//			}
+			List<BusinessInfo> resList = new ArrayList<>();
+			while(!queue.isEmpty()) {
+				resList.add(queue.poll());
+			}
+			// Medium
+//			int size = queue.size();
+//			int midIndex1;
+//			if (size % 2 == 0) {
+//				midIndex1 = size / 2 - 1;
+//			} else {
+//				midIndex1 = size / 2;
+//			}
+//			for (int i = 0; i < midIndex1; i++) {
+//				queue.poll();
+//			}
+//			double mid;
+//			if (size % 2 == 0) {
+//				mid = (queue.poll().rating + queue.poll().rating) / 2;
+//			} else {
+//				mid = queue.poll().rating;
+//			}
+//			return mid;
+			return resList;
+	}
+}
---
+++ src/happycoding2018/StringCompression.java
@@ -0,0 +1,5 @@
+package happycoding2018;
+
+public class StringCompression {
+
+}
