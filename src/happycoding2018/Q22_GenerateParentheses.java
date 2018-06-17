ec909fd6b106eb7da5449916ac427eaf1a78dc68
Jilin Liu <jilin@oomba.com>
2018-06-16 23:33:26 -0700

    24. Swap Nodes in Pairs

--- src/happycoding2018/Q22_GenerateParentheses.java
+++ src/happycoding2018/Q22_GenerateParentheses.java
@@ -1,5 +1,25 @@
 package happycoding2018;
 
-public class Q22_GenerateParentheses {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Q22_GenerateParentheses {
+	public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        helper(list, "", n, 0, 0);
+        return list;
+    }
+    
+    private void helper(List<String> list, String s, int n, int left, int right) {
+        if (s.length() == 2 * n) {
+            list.add(s);
+            return;
+        }
+        if (left < n) {
+            helper(list, s + "(", n, left + 1, right);
+        }
+        if (left > right) {
+            helper(list, s + ")", n, left, right + 1);
+        }
+    }
 }
